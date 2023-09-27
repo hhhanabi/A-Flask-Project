e6744a2dfbd5c2b61425fd84214aaf1b978847a2
Asuka <645482284@qq.com>
2023-09-27 16:57:36 +0800

    just a demo

---
+++ application/extensions.py
@@ -0,0 +1,10 @@
+from flask_jwt import JWT
+from flask_admin import Admin
+from flask_login import LoginManager
+from flask_mongoengine import MongoEngine
+
+
+db = MongoEngine()
+login_manager = LoginManager()
+admin = Admin()
+jwt = JWT()
