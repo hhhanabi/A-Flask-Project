2604e653b61de884cbd55cae7dfe894239a450ec
Asuka <645482284@qq.com>
2023-09-28 13:38:02 +0800

    login control

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
-
+from flask_login import current_user,login_user
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -21,6 +22,8 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
