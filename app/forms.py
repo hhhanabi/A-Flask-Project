5a285c5451d92bd83a94f45b05f59964a9686c38
Asuka <645482284@qq.com>
2023-10-05 18:34:16 +0800

    followers

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import _Auto, FlaskForm
+from flask_wtf import  FlaskForm
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import DataRequired,Length
 from flask_wtf import FlaskForm
