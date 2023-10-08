411dc3f2e6ad1129dda28e94430569664b638f3e
Asuka <645482284@qq.com>
2023-10-08 17:25:24 +0800

    restruct

--- microblog.py
+++ microblog.py
@@ -1,6 +1,4 @@
-from app import app, db
+from app import create_app, db
 from app.models import User, Post
 
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+app=create_app()
