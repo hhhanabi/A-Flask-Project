80693ea9848c594346252a62ff7084d742a34557
Asuka <645482284@qq.com>
2023-10-08 11:05:04 +0800

    use email to reset password

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler,RotatingFileHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
+import os
 
 app=Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +15,7 @@ db=SQLAlchemy(app)
 migrate=Migrate(app,db)
 login=LoginManager(app)
 login.login_view='login'
+mail=Mail(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -29,4 +32,15 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 from app import routes,models,errors
--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,8 @@ from app.forms import *
 from flask_login import current_user, login_required,login_user, logout_user
 from app.models import Post, User
 from werkzeug.urls import url_parse
+from app.forms import ResetPasswordRequestForm
+from app.email import send_password_reset_email
 
 @app.route('/',methods=['GET', 'POST'])
 @app.route('/index',methods=['GET', 'POST'])
@@ -129,3 +131,17 @@ def explore():
     next_url=url_for('index',page=posts.next_num) if posts.has_next else None
     prev_url=url_for('index',page=posts.prev_num) if posts.has_prev else None
     return render_template('index.html', title='Explore',posts=posts.items,next_url=next_url,prev_url=prev_url)
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html',
+                           title='Reset Password', form=form)
